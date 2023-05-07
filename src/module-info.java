ce9023b2ca4e5a77dbf5ef81a9709cbe40db48c8
Francis Dominic Fajardo <fofajardo@up.edu.ph>
2023-05-07 17:49:34 +0800

    Set up JavaFX module and project info

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module FajardoProject {
+    requires transitive javafx.base;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
+    requires transitive javafx.media;
+
+    exports miniprojtemplate;
+}
