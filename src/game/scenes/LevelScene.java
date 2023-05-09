ff4a79ae40f99ba97fdd8115f00d2ad3e67d74c6
Francis Dominic Fajardo <fofajardo@up.edu.ph>
2023-05-09 17:48:35 +0800

    Remove unused import

--- src/game/scenes/LevelScene.java
+++ src/game/scenes/LevelScene.java
@@ -16,7 +16,6 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class LevelScene implements GameScene {
